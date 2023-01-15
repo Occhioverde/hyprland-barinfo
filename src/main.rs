7e526bec9bca6be074360f4a3233e45665d2c657
Riccardo Sacchetto <rsacchetto@nexxontech.it>
2023-01-15 21:23:33 +0100

    Add ability to recover from errors

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,29 @@ fn main() -> anyhow::Result<()> {
     let hypr_sig = env::var("HYPRLAND_INSTANCE_SIGNATURE")?;
 
     let event_socket = UnixStream::connect(format!("/tmp/hypr/{}/.socket2.sock", hypr_sig))?;
-
     let curr_opts = opts::opts();
-    let mut ipcreader = IPCReader::new(curr_opts.mode, event_socket)?;
+    let mut ipcreader = IPCReader::new(curr_opts.mode.clone(), event_socket)?;
 
     println!("{}", ipcreader.force_out());
 
     loop {
-        if let Some(printable_string) = ipcreader.read()? {
-            println!("{}", printable_string);
+        if let Ok(read_res) = ipcreader.read() {
+            if let Some(printable_string) = read_res {
+                println!("{}", printable_string);
+            }
+        } else {
+            eprintln!("Recovering from error...");
+
+            // Drop the corrupt IPC reader
+            drop(ipcreader);
+
+            // Establish a new socket connection
+            let event_socket = UnixStream::connect(format!("/tmp/hypr/{}/.socket2.sock", hypr_sig))?;
+            // Reset the IPC reader
+            ipcreader = IPCReader::new(curr_opts.mode.clone(), event_socket)?;
+
+            // Resume printing
+            println!("{}", ipcreader.force_out());
         }
     }
 }
